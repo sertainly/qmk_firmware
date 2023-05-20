f348b9921f864ca12b591cfa7e72397265c8da99
Simon <simonwsommer@gmail.com>
2023-05-20 21:30:26 +0200

    Disable OLED

--- keyboards/splitkb/kyria/keymaps/canary/rules.mk
+++ keyboards/splitkb/kyria/keymaps/canary/rules.mk
@@ -1,4 +1,4 @@
-OLED_ENABLE = yes
+OLED_ENABLE = no
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = yes    # Enables the use of one or more encoders
 CAPS_WORD_ENABLE = yes  # For activation see config.h
